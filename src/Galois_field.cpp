5e4bea49b971fcc585846b2265f59c8a3abbcd62
thomaswknd <thomaswknd@gmail.com>
2022-11-20 18:58:31 +0300

    almost working Galois_field

--- src/Galois_field.cpp
+++ src/Galois_field.cpp
@@ -1,7 +1,8 @@
 #include <vector>
-#include <math.h>
-#include "Polynomial.hpp"
+#include <map>
 #include "Galois_field.hpp"
+#include "Polynomial.hpp"
+
 
 // int times(int x, int y) {
 //     int s = 0;
@@ -11,6 +12,101 @@
 //     return (x-1)*s;
 // }
 
+Galois_field::Galois_field(int y, int x = 2): base(x), pow(y) { // base pow
+    constructPole();
+    constructGroup();
+    constructMultiGroup();
+    constructSumTable();
+    constructMultiTable();
+}
+
+void Galois_field::constructPole() {
+    for (int i=0; i<this->base; i++) {
+        this->pole.push_back(i);
+    }
+}
+
+void Galois_field::constructGroup() {
+    Counter c{this->pow,this->base};
+    for (int k=0; k<c.maxInc(); k++) {
+        Counter c1 = c;
+        this->group.push_back(Polynomial{c1}); // 000 y = 3   0000 y = 4
+        ++c;
+    }
+}
+
+// void Galois_field::constructIrrPolynomial() {
+//     Counter c{this->pow, this->base};
+//     c + 1;
+// }
+
+void Galois_field::constructMultiGroup() {
+    this->multiGroup = this->group;
+    this->multiGroup.erase(this->multiGroup.begin());
+}
+
+void Galois_field::constructSumTable() {
+    for (auto j: this->group) {
+        // auto y = std::map<Polynomial, Polynomial> {j,j};
+        // sumTable.insert({j, y});
+        sumTable.insert({j, std::map<Polynomial,Polynomial>()});
+        sumTable[j].insert(std::make_pair(j,j));
+        // sumTable.insert(std::make_pair(j,std::map<Polynomial, Polynomial>(j,j)));
+        for (auto i: this->group) {
+            // sumTable[j] = {i, i};
+            // sumTable[j].insert(std::make_pair(i,i));
+            // auto elem = sum(j, i);
+            // inSumTable(j, i, elem);
+        }
+    }
+}
+
+void Galois_field::constructMultiTable() {
+    // for (auto j: this->group) {
+    //     for (auto i: this->group) {
+    //         auto elem = multi(j, i);
+    //         if (elem == Polynomial{Monomial{1,0}})
+    //             this->revVector.insert({i,j});
+    //         inMultiTable(j, i, elem);
+    //     }
+    // }
+}
+
+// void Galois_field::constructRevVector() {
+
+// }
+
+Polynomial Galois_field::atSumTable(Polynomial first, Polynomial second) {
+    // auto &a = this->sumTable.at(first);
+    // return a.at(second);
+}
+
+Polynomial Galois_field::atMultiTable(Polynomial first, Polynomial second) {
+    // auto &a = this->multiTable.at(first);
+    // return a.at(second);
+}
+
+void Galois_field::inSumTable(Polynomial first, Polynomial second, Polynomial elem) {
+    // auto &a = this->sumTable.at(first);
+    // a.insert({second, elem});
+
+    // this->sumTable[first][second] = elem;
+}
+
+void Galois_field::inMultiTable(Polynomial first, Polynomial second, Polynomial elem) {
+    // auto &a = this->multiTable.at(first);
+    // a.insert({second, elem});
+
+    // this->multiTable[first][second] = elem;
+}
+
+// Polynomial Galois_field::sum(Polynomial a, Polynomial b) {
+
+// }
+
+// Polynomial Galois_field::multi(Polynomial a, Polynomial b) {
+
+// }
 // Galois_field::Galois_field(int y, int x = 2) {
 //     for (int i=0; i<x; i++) {
 //         this->pole.push_back(i);
--- src/Galois_field.hpp
+++ src/Galois_field.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include <math.h>
+#include <map>
 #include "Polynomial.hpp"
 
 // int repeatFun(int x, int y) {
@@ -12,25 +12,48 @@
 // }
 
 class  Galois_field { // Поле Галуа
-    // int p = 0;
-    // int n = 0;
-    // sumTable;
-    // mulTable;
-    // revVector;
+    int base = 0;
+    int pow = 0;
+    std::map<Polynomial, std::map<Polynomial, Polynomial>> sumTable;
+    std::map<Polynomial, std::map<Polynomial, Polynomial>> multiTable;
+    std::map<Polynomial, Polynomial> revVector;
     public:
+    // Polynomial irrPolynomial{};
     std::vector<int> pole;
     std::vector<Polynomial> group;
+    std::vector<Polynomial> multiGroup;
     // Galois_field(int y, int x = 2);
-    Galois_field(int y, int x = 2) {
-    for (int i=0; i<x; i++) {
-        this->pole.push_back(i);
-    }
-    Counter c{y,x};
-    for (int k=0; k<c.maxInc(); k++) {
-        Counter c1 = c;
-        this->group.push_back(Polynomial{c1}); // 000 y = 3   0000 y = 4
-        ++c;
-    }
-}
+    // Galois_field(int y, int x = 2): base(x), pow(y) { // base pow
+    // constructPole();
+    // constructGroup();
+    // constructMultiGroup();
+    // constructSumTable();
+    // constructMultiTable();
+    // constructRevVector();
+    // for (int i=0; i<x; i++) {
+    //     this->pole.push_back(i);
+    // Counter c{y,x};
+    // for (int k=0; k<c.maxInc(); k++) {
+    //     Counter c1 = c;
+    //     this->group.push_back(Polynomial{c1}); // 000 y = 3   0000 y = 4
+    //     ++c;
+    // }
+    // this->multygroup = this->group;
+    // this->multygroup.erase(this->multygroup.begin());
+// }
+    Galois_field(int y, int x);
     ~Galois_field() {}
+    void constructPole();
+    void constructGroup();
+    // void constructIrrPolynomial();
+    void constructMultiGroup();
+    void constructSumTable();
+    void constructMultiTable();
+    // void constructRevVector();
+    Polynomial atSumTable(Polynomial first, Polynomial second);
+    Polynomial atMultiTable(Polynomial first, Polynomial second);
+    void inSumTable(Polynomial first, Polynomial second, Polynomial elem);
+    void inMultiTable(Polynomial first, Polynomial second, Polynomial elem);
+    Polynomial sum(Polynomial a, Polynomial b);
+    Polynomial multi(Polynomial a, Polynomial b);
 };
