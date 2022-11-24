21a9caf1b08af6f8f8f9b59114897ab52698bbfa
thomaswknd <thomaswknd@gmail.com>
2022-11-24 23:59:45 +0300

    added locale

--- src/toUpperCase.cpp
+++ src/toUpperCase.cpp
@@ -1,7 +1,9 @@
 #include "toUpperCase.hpp"
+#include <locale>
 
 std::string toUpperCase(std::string str)
 {
+    std::locale locale("");
     std::transform(str.begin(), str.end(), str.begin(), ::toupper);
     return str;
 }
